dc3b6d6a4390da13e47f4f0d7f15a42d7674e31b
Lav-Venkat <inukurthivenkatesh@gmail.com>
2024-09-19 22:16:45 +0530

    loops count and for loop

--- loops/outputs.tf
+++ loops/outputs.tf
@@ -1,4 +1,18 @@
-output "public_ip" {
-    value = aws_instance.terraform_loop.public_ip
-  
+# output "public_ip" {
+#     value = [aws_instance.terraform_loop[*].public_ip]
+#     sensitive = false
+#     description = "This is the public ip of instance created"     
+# }
+
+# output "instance_info" {
+#     value = aws_instance.terraform_loop  
+# }
+
+output "instance_details" {
+  value = [
+    for instance in aws_instance.terraform_loop : {
+      name      = instance.tags["Name"]
+      public_ip = instance.public_ip
+    }
+  ]
 }

4bfc2f8090809b83975a7ed1524e31e935e7f17d
Lav-Venkat <inukurthivenkatesh@gmail.com>
2024-09-19 21:30:36 +0530

    Added security group id variable in output

--- conditions/outputs.tf
+++ conditions/outputs.tf
@@ -3,3 +3,10 @@ output "public_ip" {
     sensitive = false
     description = "This is the public ip of instance created"     
 }
+
+output "security_group_id" {
+    value = aws_security_group.allow_all_conditions_tf.id
+    sensitive = false
+    description = "This is the security group id"
+  
+}

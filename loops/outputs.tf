9732f06c82ba58437ef833f6bd4f9448aa68a1c5
Lav-Venkat <inukurthivenkatesh@gmail.com>
2024-09-19 21:51:17 +0530

    loops terraform

--- loops/outputs.tf
+++ loops/outputs.tf
@@ -1,4 +1,4 @@
-output "public_ip" {
-    value = aws_instance.terraform_loop[count.index].public_ip
+# output "public_ip" {
+#     value = aws_instance.terraform_loop[count.index].public_ip
   
-}
+# }

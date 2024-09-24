35e4623733ef78d952a34b7468fd1b05b501358a
Lav-Venkat <inukurthivenkatesh@gmail.com>
2024-09-24 21:32:04 +0530

    terraform VPC

--- ec2-module-demo/ec2.tf
+++ ec2-module-demo/ec2.tf
@@ -2,6 +2,6 @@ module "ec2" {
     source = "../terraform-aws-ec2"
 
     ami_id = var.ami_id
-    instance_type = "t3.large"
+    instance_type = "t3.medium"
     security_group_ids = ["sg-088988091b81eae5c"]  
 }
--- terraform-aws-ec2/variable.tf
+++ terraform-aws-ec2/variable.tf
@@ -10,7 +10,7 @@ variable "instance_type" {
     default = "t3.micro"
     type = string
     validation {
-        condition = contains(["t3.micro", "t3.medium", "t3.small"])
+        condition = contains(["t3.micro", "t3.medium", "t3.small"],var.instance_type)
         error_message = "Instance type can only be one of t3.micro or t3.medium or t3.small"
     }  
 }
